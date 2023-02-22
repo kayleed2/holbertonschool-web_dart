70bfbee2db388b16631510e28089b036cf65ddba
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 10:21:09 -0600

    Task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,19 +1,12 @@
-String name = "", id = "";
-
 void outer(String name, String id) {
-  // set the name and id to global variables to be used by the inner() function
-  outer.name = name;
-  outer.id = id;
-  // call the inner() function to generate the output
+  String inner() {
+    // extract the first letter of the first and last name
+    String firstName = name.split(" ")[0].substring(0, 1);
+    String lastName = name.split(" ")[1].substring(0, 1);
+    // concatenate the output string
+    String output = "Hello Agent $lastName.$firstName your id is $id\n";
+    // return the output
+    return output;
+  }
   print(inner());
-}
-
-String inner() {
-  // extract the first letter of the first and last name
-  String firstName = name.split(" ")[0].substring(0, 1);
-  String lastName = name.split(" ")[1].substring(0, 1);
-  // concatenate the output string
-  String output = "Hello Agent $lastName.$firstName your id is $id\n";
-  // return the output
-  return output;
 }
