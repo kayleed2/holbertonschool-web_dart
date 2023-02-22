49d17b9c5174a1743151acbf822d06e5c1803c5d
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 11:04:51 -0600

    Task 1

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -1,10 +1,17 @@
 class User {
-  String name = "";
+  String name;
   int age;
   double height;
+  User({
+    required this.name,
+    required this.age,
+    required this.height,
+  });
   Map<String, dynamic> toJson() {
+    return {
     'name': name,
     'age': age,
     'height': height,
     };
+  }
 }
