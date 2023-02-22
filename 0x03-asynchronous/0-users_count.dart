474d7cdf78ceed14a9eecf96cb43078ba62622a7
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 12:21:09 -0600

    Task 0

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,3 +1,12 @@
+Future<int> fetchUsersCount() => Future.delayed(
+      const Duration(seconds: 2),
+      () => 19,
+    );
+
+main() async {
+  await usersCount();
+}
+
 Future<void> usersCount() {
   try {
     int count = fetchUsersCount();
