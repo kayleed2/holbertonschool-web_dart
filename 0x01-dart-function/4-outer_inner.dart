0519e78c134b7b6540d3227ed6d5eb84a88da94f
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 10:21:57 -0600

    Task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -2,7 +2,7 @@ void outer(String name, String id) {
   String inner() {
     // extract the first letter of the first and last name
     String firstName = name.split(" ")[0].substring(0, 1);
-    String lastName = name.split(" ")[1].substring(0, 1);
+    String lastName = name.split(" ")[1];
     // concatenate the output string
     String output = "Hello Agent $lastName.$firstName your id is $id\n";
     // return the output
