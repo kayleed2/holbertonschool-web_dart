6b4ba89150afad3dcba0f60b525ba8204917b92f
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 13:02:47 -0600

    Task 3

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 Future<String> fetchUserData() => Future.delayed(
       const Duration(seconds: 2),
       () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "3.14 Battery street"}',
     );
 
 Future<bool> checkCredentials() =>
