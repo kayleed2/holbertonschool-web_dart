5ec4dd0b775070eabed645cce7e450f6e14036e9
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 10:51:40 -0600

    Task 10

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,3 +1,19 @@
+bool isPalindrome(String s) {
+  if (s == null || s.length < 3) {
+    return false;
+  }
+  int left = 0;
+  int right = s.length - 1;
+  while (left < right) {
+    if (s[left] != s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
 String longestPalindrome(String s) {
   if (s == null || s.length < 3) {
     return null;
