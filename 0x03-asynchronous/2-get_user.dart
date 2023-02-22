584f7b0bfb337a29757b53e2fd6728dec936d90f
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 12:29:19 -0600

    Task 2

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,18 @@
+Future<String> fetchUser() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
+main() async {
+  getUser();
+}
+
+Future<void> getUser() {
+   try {
+		print(await fetchUser());
+	} catch (error) {
+		print("error caught: $error");
+	}
+}
