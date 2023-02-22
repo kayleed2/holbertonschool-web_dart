2a080b82aba584a15b1d1e49c9b832a62ad74899
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 10:46:30 -0600

    Task 8

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,14 +1,11 @@
 String longestUniqueSubstring(String str) {
-    if (str == null || str.isEmpty) {
-        return '';
-        }
     int n = str.length;
     int maxLength = "";
     int position = "";
     int at = "";
-    for (int i = 0, j < n; ++j) {
-        for (int y = x; y < str.length; ++y) {
-            position = str.substring(x, y + 1);
+    for (int i = 0, i < n; ++i) {
+        for (int x = i; x < str.length; ++x) {
+            position = str.substring(i, x + 1);
             at = position.split('').toSet().join('');
             if ((position.length > maxLength.length) && position == at) {
                 maxLength = position;
