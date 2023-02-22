ffb0fd9898fbbbe4646eeb9017ad2bb19c28bb21
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 12:15:02 -0600

    Task 0

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,14 +1,8 @@
-Future<void> usersCount() async {
+Future<void> usersCount() {
   try {
     int count = await fetchUsersCount();
-    print('Number of users: $count');
+    print('$count');
   } catch (error) {
     print('Error fetching user count: $error');
   }
-}
-
-Future<int> fetchUsersCount() async {
-  // Simulate fetching data from an API
-  await Future.delayed(Duration(seconds: 1));
-  return 10; // Return a dummy value for the number of users
 }
