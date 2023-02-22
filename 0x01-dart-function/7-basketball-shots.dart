31b0f30600cbc9a9dabbc68b3d4a72da4fc5d657
Kaylee Dao <kayleed2@gmail.com>
2023-02-22 10:55:23 -0600

    Task 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,27 +1,9 @@
 int whoWins(Map<String, int> team1, Map<String, int> team2) {
-  int team1Points = 0;
-  int team2Points = 0;
-  // Calculate team1 points
-  if (team1.containsKey('free throw')) {
-    team1Points += team1['free throw'];
-  }
-  if (team1.containsKey('2 pointer')) {
-    team1Points += team1['2 pointer'] * 2;
-  }
-  if (team1.containsKey('3 pointer')) {
-    team1Points += team1['3 pointer'] * 3;
-  }
-  // Calculate team2 points
-  if (team2.containsKey('free throw')) {
-    team2Points += team2['free throw'];
-  }
-  if (team2.containsKey('2 pointer')) {
-    team2Points += team2['2 pointer'] * 2;
-  }
-  if (team2.containsKey('3 pointer')) {
-    team2Points += team2['3 pointer'] * 3;
-  }
-  // Compare points and return winner
+  // calculate the total number of points for each team
+  int team1Points = (team1["free throws"] ?? 0) + (team1["2 pointers"] ?? 0) * 2 + (team1["3 pointers"] ?? 0) * 3;
+  int team2Points = (team2["free throws"] ?? 0) + (team2["2 pointers"] ?? 0) * 2 + (team2["3 pointers"] ?? 0) * 3;
+  
+  // determine the winning team
   if (team1Points > team2Points) {
     return 1;
   } else if (team1Points < team2Points) {
